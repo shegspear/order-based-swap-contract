cc940cde3ef6a6b9bae584eacb7d7ddb39bf92f3
shegspear <shegspear@gmail.com>
2024-09-19 16:40:53 +0100

    fixed Swap contract bug

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -1,5 +1,13 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// Sources flattened with hardhat v2.22.10 https://hardhat.org
+
+// SPDX-License-Identifier: SEE
+
+// File contracts/Swap.sol
+
+// Original license: SPDX_License_Identifier: SEE
 pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Swap{
 
@@ -9,10 +17,10 @@ contract Swap{
     address public spb3CX;
     address public crd3CX;
 
-    uint16 web3CXSwap = 2;
+    uint8 web3CXSwap = 2;
     uint16 edd3CXSwap = 4;
     uint16 spb3CXSwap = 2;
-    uint16 crd3CXwap = 4;
+    uint16 crd3CXSwap = 4;
 
     error CanNotSwapSimilarToken();
     error InsufficientAmountIn();
@@ -34,10 +42,35 @@ contract Swap{
         address tokenA,
         address tokenB,
         uint256 amountIn,
-        uint256 amountOut
+        uint16 amountOut
     ) external {
 
-        
+        // IERC20(tokenA).approve(address(this), amountIn);
+
+        IERC20(tokenA).transferFrom(msg.sender, address(this), amountIn);
+
+        uint16 _amount;
+
+        if(tokenB == web3CX) {
+            _amount = amountOut / web3CXSwap;
+            require(IERC20(tokenB).balanceOf(address(this)) >= _amount, "Insufficient fund");
+            IERC20(tokenB).transfer(msg.sender, _amount);
+
+        } else if(tokenB == edd3CX) {
+            _amount = amountOut / edd3CXSwap;
+            require(IERC20(tokenB).balanceOf(address(this)) >= _amount, "Insufficient fund");
+            IERC20(tokenB).transfer(msg.sender, _amount);
+
+        } else if(tokenB == spb3CX) {
+            _amount = amountOut / spb3CXSwap;
+            require(IERC20(tokenB).balanceOf(address(this)) >= _amount, "Insufficient fund");
+            IERC20(tokenB).transfer(msg.sender, _amount);
+
+        } else if(tokenB == crd3CX) {
+            _amount = amountOut / crd3CXSwap;
+            require(IERC20(tokenB).balanceOf(address(this)) >= _amount, "Insufficient fund");
+            IERC20(tokenB).transfer(msg.sender, _amount); 
+        }
 
     }
 
